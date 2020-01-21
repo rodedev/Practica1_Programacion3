a395bf5771d086da699776b589d789c22b3ac860
rddipre <rddipre@hotmail.com>
2020-01-21 03:13:33 -0400

    Practica 1 de ASP.NET de programacion 3

---
+++ Practica1/Controllers/PersonalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica1.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // GET: Personal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Asignaturas()
+        {
+            return View();
+        }
+
+        public ActionResult Pasatiempos()
+        {
+            return View();
+        }
+
+        public ActionResult PeliculasFaboritas()
+        {
+            return View();
+        }
+    }
+}
